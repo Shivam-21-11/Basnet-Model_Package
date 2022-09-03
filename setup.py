2978262b7a0499fe1364c9f32af275e836d07b9f
Shivam-21-11 <shivamsingh2111@gmail.com>
2022-09-03 10:39:40 +0530

    init

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(
-    name="BASNETMODEL",
+    name="BASNET",
     version="0.0.1",
     description="BASNET model created using tensorflow.",
     long_description=readme(),
